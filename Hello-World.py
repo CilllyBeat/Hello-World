55314c0c40c5644dcb92acde1fa83e4f88bfea8d
CillyBeat <ccjo1307@gmail.com>
2018-03-23 11:41:17 +0100

    almost understand

--- Hello-World.py
+++ Hello-World.py
@@ -1,2 +1,6 @@
 print('Hello World')
 print('New line to test commit and push')
+firstName = input("What is your first name?:")
+lastName =input("what is your last name?:")
+
+print('Nice to meet you', firstName, lastName, "!")
